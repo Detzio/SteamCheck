17eb63d9e6221a9465a3d9c5e6c512fb64df1965
ItakuriUchiha <pierrehenri.luczak@gmail.com>
2025-04-17 17:10:11 +0200

    Theme fix

--- composeApp/src/desktopMain/kotlin/org/steamcheck/project/ui/theme/Theme.kt
+++ composeApp/src/desktopMain/kotlin/org/steamcheck/project/ui/theme/Theme.kt
@@ -3,19 +3,19 @@ package org.steamcheck.project.ui.theme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
+private val DarkColorPalette = darkColorScheme(
     primary = Color(0xFF1A9FFF),
-    primaryVariant = Color(0xFF0B76CC),
     secondary = Color(0xFF03DAC5)
 )
 
-private val LightColorPalette = lightColors(
+private val LightColorPalette = lightColorScheme(
     primary = Color(0xFF1A9FFF),
-    primaryVariant = Color(0xFF0B76CC),
     secondary = Color(0xFF03DAC5)
 )
 
@@ -31,7 +31,7 @@ fun SteamCheckTheme(
     }
 
     MaterialTheme(
-        colors = colors,
+        colorScheme = colors,
         content = content
     )
 }
