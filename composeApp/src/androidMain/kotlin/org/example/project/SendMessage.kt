4f5c92dc35a5b541ed4d073d9dd535caef9dd279
ItakuriUchiha <pierrehenri.luczak@gmail.com>
2025-04-17 14:33:07 +0200

    Revert "Fusion des historiques"
    This reverts commit 815f51579d53d5ee86693d64ee1b09bb06fce052, reversing
    changes made to 26cb7db63fc44d7507b3a3d121d829056d4fc3f5.

--- composeApp/src/androidMain/kotlin/org/example/project/SendMessage.kt
+++
@@ -1,5 +0,0 @@
-package org.example.project
-
-actual fun sendMessage(){
-    println("Hello From Android")
-}
--- composeApp/src/nativeMain/kotlin/org/example/project/Platform.native.kt
+++
@@ -1,5 +0,0 @@
-package org.example.project
-
-actual fun sendMessage() : String {
-    return "Hello From Native"
-}
